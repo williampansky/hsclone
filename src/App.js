acd43a222eea89af7b6070ef9073f14a03ddee92
William Pansky <williampansky@gmail.com>
2020-01-21 21:57:03 -0600

    removed mousePosition tracking

--- src/App.js
+++ src/App.js
@@ -16,37 +16,12 @@ import YourBoard from 'features/yourBoard/YourBoard';
 import TheirBoard from 'features/layout/TheirBoard.container';
 
 export default function App() {
-  const [mouseX, setMouseX] = React.useState(0);
-  const [mouseY, setMouseY] = React.useState(0);
-  const [domElement, setDomElement] = React.useState(null);
-
-  const handleMouseMove = event => {
-    const {
-      nativeEvent: {
-        offsetX,
-        offsetY,
-        toElement: {
-          dataset: { file }
-        }
-      }
-    } = event;
-
-    offsetX && setMouseX(offsetX);
-    offsetY && setMouseY(offsetY);
-    file && setDomElement(file);
-  };
-
   return (
     <React.Fragment>
       <WindowSizer>
-        <DebugBar
-          mousePositionX={mouseX}
-          mousePositionY={mouseY}
-          target={domElement}
-        />
+        <DebugBar />
         <Header />
         <DndProvider backend={HTML5Backend}>
-          {/* <Board onMouseMove={event => handleMouseMove(event)}> */}
           <Board>
             <TheirBoard />
             <YourBoard />
