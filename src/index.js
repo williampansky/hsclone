b2bf3c1a6d9102145f0a5d244943ca1984a44649
William Pansky <williampansky@gmail.com>
2020-01-22 12:30:39 -0600

    added normalize.css dep

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'store';
 import App from './App';
+import 'normalize.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
