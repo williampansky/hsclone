6cf8aa7cd454bdc75223541401753e0b8145757f
William Pansky <williampansky@gmail.com>
2020-02-03 10:53:25 -0600

    integrated random playOrder on game init. also integrated energy incrementation and fulfillment via onBegin turn method

--- src/lib/game.js
+++ src/lib/game.js
@@ -1,6 +1,12 @@
 import { drawCard, playMinionCard, playSpellCard } from './moves/card-moves';
+import {
+  incrementCurrentEnergy,
+  incrementTotalEnergy,
+  setCurrentEnergy,
+  setTotalEnergy
+} from './moves/energy-moves';
 
-let StripSecrets = (G, playerID) => ({
+let stripSecrets = (G, playerID) => ({
   ...G,
   players: {
     [playerID]: G.players[playerID]
@@ -56,39 +62,66 @@ export const HSclone = {
     energy: {
       0: {
         current: 0,
-        total: 10
+        total: 0
       },
       1: {
         current: 0,
-        total: 10
+        total: 0
       }
     }
   }),
 
   // prettier-ignore
   moves: {
-    moveThatUsesSecret: {
-      
-      playMinionCard: (G, ctx, slotNumber, card) => playMinionCard(G, ctx, slotNumber, card),
-      playSpellCard: (G, ctx, card, target = null) => playSpellCard(G, ctx, card, target),
+    // server-side moves (e.g. `client: false`)
+    drawCard: { client: false, move: (G, ctx, card) => drawCard(G, ctx, card) },
+    playMinionCard: { client: false, move: (G, ctx, slotNumber, card) => playMinionCard(G, ctx, slotNumber, card) },
+    playSpellCard: { client: false, move: (G, ctx, card, target = null) => playSpellCard(G, ctx, card, target) },
 
-      client: false
-    },
-
-    drawCard: {
-      move: (G, ctx, card) => drawCard(G, ctx, card),
-      client: false
-    },
-
-    // drawCard: (G, ctx, card) => drawCard(G, ctx, card),
-    // playMinionCard: (G, ctx, slotNumber, card) => playMinionCard(G, ctx, slotNumber, card),
-    // playSpellCard: (G, ctx, card, target = null) => playSpellCard(G, ctx, card, target),
-
-    endTurn: (G, ctx) => {}
+    // client-side moves
+    // setCurrentEnergy: (G, ctx, player, amount) => setCurrentEnergy(G, ctx, player, amount),
+    // setTotalEnergy: (G, ctx, player, amount) => setTotalEnergy(G, ctx, player, amount),
   },
 
-  // Function that allows you to tailor the game state to a specific player.
+  /**
+   * This method uses the `stripSecrets` function to hide
+   * the opponent player's hand and deck data from your client.
+   * @requires stripSecrets
+   */
   playerView: (G, ctx, playerID) => {
-    return StripSecrets(G, playerID);
+    return stripSecrets(G, playerID);
+  },
+
+  // prettier-ignore
+  turn: {
+    order: {
+      // Get the initial value of playOrderPos.
+      // This is called at the beginning of the phase.
+      first: (G, ctx) => 0,
+  
+      // Get the next value of playOrderPos.
+      // This is called at the end of each turn.
+      // The phase ends if this returns undefined.
+      next: (G, ctx) => (ctx.playOrderPos + 1) % ctx.numPlayers,
+  
+      // OPTIONAL:
+      // Override the initial value of playOrder.
+      // This is called at the beginning of the game / phase.
+      playOrder: (G, ctx) => ctx.random.Shuffle(ctx.playOrder),
+    },
+    onBegin: (G, ctx) => {
+      /**
+       * Increments the `total` energy of the `currentPlayer`
+       * by one; but not more than ten.
+       */
+      if (G.energy[ctx.currentPlayer].total !== 10)
+        G.energy[ctx.currentPlayer].total++;
+      
+      /**
+       * Then, sets the `current` value to the total; which allows
+       * the `currentPlayer` to spend said energy on card play functions.
+       */
+      G.energy[ctx.currentPlayer].current = G.energy[ctx.currentPlayer].total;
+    },
   }
 };
--- src/lib/moves/energy-moves.js
+++ src/lib/moves/energy-moves.js
@@ -1,1 +1,24 @@
 import { add, subtract } from 'mathjs';
+
+export const incrementCurrentEnergy = (G, ctx, playerID) => {
+  if (Number(ctx.currentPlayer) === Number(playerID))
+    G.energy[ctx.currentPlayer].current =
+      G.energy[ctx.currentPlayer].current + 1;
+  return ctx.events.endPhase();
+};
+
+export const incrementTotalEnergy = (G, ctx, playerID) => {
+  if (Number(ctx.currentPlayer) !== Number(playerID))
+    G.energy[ctx.currentPlayer].total = G.energy[ctx.currentPlayer].total + 1;
+  return ctx.events.endPhase();
+};
+
+export const setCurrentEnergy = (G, ctx, player, amount) => {
+  const { energy } = G;
+  energy[player].current = amount;
+};
+
+export const setTotalEnergy = (G, ctx, player, amount) => {
+  const { energy } = G;
+  energy[player].total = amount;
+};
