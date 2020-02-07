f79ad07713d920dfe6c2d8a12c3a53bf99820fc1
William Pansky <williampansky@gmail.com>
2020-02-07 12:11:57 -0600

    migrated individual energy move tests to energy-moves.test suite file

--- src/__tests__/unit/setCurrentEnergy.test.js
+++
@@ -1,8 +0,0 @@
-const esmImport = require('esm')(module);
-const { setCurrentEnergy } = esmImport('../../lib/moves/energy-moves');
-
-test(`set player 0's current energy to provided amount`, () => {
-  const G = { energy: { 0: { current: 0 } } };
-  const result = setCurrentEnergy(G, 0, 10);
-  expect(result).toBe(10);
-});
--- src/__tests__/unit/setTotalEnergy.test.js
+++
@@ -1,8 +0,0 @@
-const esmImport = require('esm')(module);
-const { setTotalEnergy } = esmImport('../../lib/moves/energy-moves');
-
-test(`set player 0's total energy to provided amount`, () => {
-  const G = { energy: { 0: { total: 0 } } };
-  const result = setTotalEnergy(G, 0, 10);
-  expect(result).toBe(10);
-});
