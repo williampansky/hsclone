73a30e019d071134dabdcc4207286414f63c771b
William Pansky <williampansky@gmail.com>
2020-02-09 12:08:50 -0600

    fixed the orb spell cast

--- src/lib/moves/spell-moves.js
+++ src/lib/moves/spell-moves.js
@@ -3,7 +3,7 @@ import { castTheOrb } from '../spells/cast-the-orb';
 export const playSpellByCardId = (G, ctx, cardId) => {
   // prettier-ignore
   switch (cardId) {
-    case 'THE_ORB':   return castTheOrb(G, ctx);
-    default:          return null;
+    case 'GAME_001':    return castTheOrb(G, ctx);
+    default:            return null;
   }
 };
