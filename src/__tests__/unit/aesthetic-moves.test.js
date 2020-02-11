22549eb72b8ce3b47bf121408d203ec981e731b8
William Pansky <williampansky@gmail.com>
2020-02-11 10:01:03 -0600

    unit testing for aesthetic-moves

---
+++ src/__tests__/unit/aesthetic-moves.test.js
@@ -0,0 +1,168 @@
+const esmImport = require('esm')(module);
+const { hoverOverCardInHand, selectPlayableCard } = esmImport(
+  '../../lib/moves/aesthetic-moves'
+);
+
+/**
+ * aesthetic-moves::hoverOverCardInHand() - w/ index
+ */
+test(`with an index provided, sets the currentPlayer's card hover index`, () => {
+  const G = {
+    hoveringCardIndex: {
+      '0': null,
+      '1': null
+    }
+  };
+
+  const ctx = {
+    currentPlayer: '1'
+  };
+
+  hoverOverCardInHand(G, ctx, 4);
+
+  expect(G).toEqual({
+    hoveringCardIndex: {
+      '0': null,
+      '1': 4
+    }
+  });
+});
+
+/**
+ * aesthetic-moves::hoverOverCardInHand() - w/o index
+ */
+test(`without an index & card provided, sets the currentPlayer's card hover to null`, () => {
+  const G = {
+    hoveringCardIndex: {
+      '0': 2,
+      '1': null
+    }
+  };
+
+  const ctx = {
+    currentPlayer: '0'
+  };
+
+  hoverOverCardInHand(G, ctx);
+
+  expect(G).toEqual({
+    hoveringCardIndex: {
+      '0': null,
+      '1': null
+    }
+  });
+});
+
+/**
+ * aesthetic-moves::selectPlayableCard() - w/o index
+ */
+test(`without an index & card provided, sets the currentPlayer's selectedCardIndex & selectedCardObject to null`, () => {
+  const G = {
+    players: {
+      '0': {
+        hand: [
+          'CARD',
+          'CARD',
+          {
+            id: 'CARD_OBJECT'
+          }
+        ]
+      }
+    },
+    selectedCardIndex: {
+      '0': 2,
+      '1': null
+    },
+    selectedCardObject: {
+      '0': {
+        id: 'CARD_OBJECT'
+      },
+      '1': null
+    }
+  };
+
+  const ctx = {
+    currentPlayer: '0'
+  };
+
+  selectPlayableCard(G, ctx);
+
+  expect(G).toEqual({
+    players: {
+      '0': {
+        hand: [
+          'CARD',
+          'CARD',
+          {
+            id: 'CARD_OBJECT'
+          }
+        ]
+      }
+    },
+    selectedCardIndex: {
+      '0': null,
+      '1': null
+    },
+    selectedCardObject: {
+      '0': null,
+      '1': null
+    }
+  });
+});
+
+/**
+ * aesthetic-moves::selectPlayableCard() - w/ index
+ */
+test(`with an index provided, sets the currentPlayer's selectedCardIndex and selectedCardObject`, () => {
+  const G = {
+    players: {
+      '0': {
+        hand: [
+          'CARD',
+          'CARD',
+          {
+            id: 'CARD_OBJECT'
+          }
+        ]
+      }
+    },
+    selectedCardIndex: {
+      '0': null,
+      '1': null
+    },
+    selectedCardObject: {
+      '0': null,
+      '1': null
+    }
+  };
+
+  const ctx = {
+    currentPlayer: '0'
+  };
+
+  selectPlayableCard(G, ctx, G.players[ctx.currentPlayer].hand[2], 2);
+
+  expect(G).toEqual({
+    players: {
+      '0': {
+        hand: [
+          'CARD',
+          'CARD',
+          {
+            id: 'CARD_OBJECT'
+          }
+        ]
+      }
+    },
+    selectedCardIndex: {
+      '0': 2,
+      '1': null
+    },
+    selectedCardObject: {
+      '0': {
+        id: 'CARD_OBJECT'
+      },
+      '1': null
+    }
+  });
+});
--- src/lib/moves/aesthetic-moves.js
+++ src/lib/moves/aesthetic-moves.js
@@ -1,7 +1,7 @@
 export const hoverOverCardInHand = (G, ctx, index) => {
-  if (index === null) {
-    G.hoveringCardIndex[0] = index;
-    G.hoveringCardIndex[1] = index;
+  if (!index || index === null) {
+    G.hoveringCardIndex[0] = null;
+    G.hoveringCardIndex[1] = null;
   } else {
     Number(ctx.currentPlayer) === 0
       ? (G.hoveringCardIndex[0] = index)
@@ -15,9 +15,9 @@ export const selectPlayableCard = (G, ctx, card, index) => {
 };
 
 export const selectPlayableCardIndex = (G, ctx, index) => {
-  if (index === null) {
-    G.selectedCardIndex[0] = index;
-    G.selectedCardIndex[1] = index;
+  if (!index || index === null) {
+    G.selectedCardIndex[0] = null;
+    G.selectedCardIndex[1] = null;
   } else {
     Number(ctx.currentPlayer) === 0
       ? (G.selectedCardIndex[0] = index)
@@ -26,9 +26,9 @@ export const selectPlayableCardIndex = (G, ctx, index) => {
 };
 
 export const selectPlayableCardObject = (G, ctx, card) => {
-  if (card === null) {
-    G.selectedCardObject[0] = card;
-    G.selectedCardObject[1] = card;
+  if (!card || card === null) {
+    G.selectedCardObject[0] = null;
+    G.selectedCardObject[1] = null;
   } else {
     Number(ctx.currentPlayer) === 0
       ? (G.selectedCardObject[0] = card)
