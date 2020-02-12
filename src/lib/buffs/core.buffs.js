a9fb54704b6f8c713065d40278572697b07c1627
William Pansky <williampansky@gmail.com>
2020-02-11 23:06:44 -0600

    added the rest of the core buffs, consolidated to a single function

--- src/lib/buffs/core.buffs.js
+++ src/lib/buffs/core.buffs.js
@@ -1,12 +1,19 @@
 export const initCoreBuff = (G, player, cardId) => {
   // prettier-ignore
   switch (cardId) {
-    case 'CORE_011':  return CORE_011(G, player);
+    case 'CORE_011':  return CORE_011_015_028_037(G, player);
+    case 'CORE_015':  return CORE_011_015_028_037(G, player);
+    case 'CORE_028':  return CORE_011_015_028_037(G, player);
+    case 'CORE_037':  return CORE_011_015_028_037(G, player);
     default:          break;
   }
 };
 
-const CORE_011 = (G, player) => {
+/**
+ * CORE_011, CORE_015, CORE_028, CORE_037
+ * Give your hero +1 Spell Damage.
+ */
+const CORE_011_015_028_037 = (G, player) => {
   const currentSpellDamage = G.buffs[player].spellDamage;
   G.buffs[player].spellDamage = currentSpellDamage + 1;
 };
