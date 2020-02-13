3bf133656ee48424c347b120e82c394dcabce16f
William Pansky <williampansky@gmail.com>
2020-02-12 22:26:39 -0600

    cleaned up minion-moves.js file; added comments

--- src/lib/moves/minion-moves.js
+++ src/lib/moves/minion-moves.js
@@ -1,71 +1,106 @@
 import { subtract } from 'mathjs';
 import { initCoreWarcry } from '../warcrys/core.warcrys';
 
+/**
+ * Disables `canAttack` of the player's board index object.
+ * @param {{}} G
+ * @param {Number} player
+ * @param {Number} index
+ */
 export const disableMinionCanAttack = (G, player, index) => {
   if (!G.boards[player][index]) return;
   return (G.boards[player][index].canAttack = false);
 };
 
+/**
+ * Enables `canAttack` of the player's board index object.
+ * @param {{}} G
+ * @param {Number} player
+ * @param {Number} index
+ */
 export const enableMinionCanAttack = (G, player, index) => {
   if (!G.boards[player][index]) return;
   return (G.boards[player][index].canAttack = true);
 };
 
+/**
+ * Disables `canBeAttacked` of the player's board index object.
+ * @param {{}} G
+ * @param {Number} player
+ * @param {Number} index
+ */
 export const disableMinionCanBeAttacked = (G, player, index) => {
   if (!G.boards[player][index]) return;
   return (G.boards[player][index].canBeAttacked = false);
 };
 
+/**
+ * Enables `canBeAttacked` of the player's board index object.
+ * @param {{}} G
+ * @param {Number} player
+ * @param {Number} index
+ */
 export const enableMinionCanBeAttacked = (G, player, index) => {
   if (!G.boards[player][index]) return;
   return (G.boards[player][index].canBeAttacked = true);
 };
 
+/**
+ * Disables `hasGuard` of the player's board index object.
+ * @param {{}} G
+ * @param {Number} player
+ * @param {Number} index
+ */
 export const disableMinionHasGuard = (G, player, index) => {
   if (!G.boards[player][index]) return;
   return (G.boards[player][index].hasGuard = false);
 };
 
+/**
+ * Enables `hasGuard` of the player's board index object.
+ * @param {{}} G
+ * @param {Number} player
+ * @param {Number} index
+ */
 export const enableMinionHasGuard = (G, player, index) => {
   if (!G.boards[player][index]) return;
   return (G.boards[player][index].hasGuard = true);
 };
 
+/**
+ * Sets `selectedMinionIndex` and `selectedMinionObject` of the current player.
+ * @param {{}} G
+ * @param {{}} ctx
+ * @param {{}} minion
+ * @param {Number} index
+ * @requires selectAttackingMinionIndex()
+ * @requires selectAttackingMinionObject()
+ */
 export const selectAttackingMinion = (G, ctx, minion, index) => {
   selectAttackingMinionIndex(G, ctx, index);
   selectAttackingMinionObject(G, ctx, minion);
 };
 
+/**
+ * Sets `selectedMinionIndex` of the current player.
+ * @param {{}} G
+ * @param {{}} ctx
+ * @param {Number} index
+ */
 export const selectAttackingMinionIndex = (G, ctx, index) => {
   Number(ctx.currentPlayer) === 0
     ? (G.selectedMinionIndex[0] = index)
     : (G.selectedMinionIndex[1] = index);
 };
 
+/**
+ * Sets `selectAttackingMinionObject` of the current player.
+ * @param {{}} G
+ * @param {{}} ctx
+ * @param {Number} index
+ */
 export const selectAttackingMinionObject = (G, ctx, minion) => {
   Number(ctx.currentPlayer) === 0
     ? (G.selectedMinionObject[0] = minion)
     : (G.selectedMinionObject[1] = minion);
-};
-
-export const initMinionWarcry = (G, ctx, slotNumber, cardId) => {
-  return initCoreWarcry(G, ctx, slotNumber, cardId);
-};
-
-export const attackPlayer = (G, ctx, player, attack) => {
-  const { selectedMinionIndex } = G;
-  const { currentPlayer } = ctx;
-
-  const selectedMinion = selectedMinionIndex[currentPlayer];
-  if (selectedMinion === null) return;
-
-  // disable MINION_ATTACKING's ability to attack
-  disableMinionCanAttack(G, currentPlayer, selectedMinion);
-
-  // reset currentPlayer's selectedMinionIndex value
-  selectAttackingMinion(G, ctx, null, null);
-
-  // subtract attack from player's health value
-  const newHealth = subtract(G.health[player], attack);
-  G.health[player] = newHealth;
 };
