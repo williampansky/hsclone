ec9476fc4a184c9ba3171c0f123a93d05163b823
William Pansky <williampansky@gmail.com>
2020-02-14 08:15:04 -0600

    created placeCardOnBoard function

--- src/lib/cards/playMinionCard.js
+++ src/lib/cards/playMinionCard.js
@@ -6,30 +6,24 @@ import { generateBoardSlotObject } from '../utils/generate-board-slot';
 import { setCurrentEnergy } from '../moves/energy-moves';
 import { moveCardToPlayedCards } from './moveCardToPlayedCards';
 import { removeCardFromHand } from './removeCardFromHand';
+import { placeCardOnBoard } from '../boards/placeCardOnBoard';
 
 /**
  * Plays the selected minion card.
  */
 export const playMinionCard = (G, ctx, index, cardId, cardCost) => {
-  const { boards, energy } = G;
+  const { energy } = G;
   const { currentPlayer } = ctx;
 
   const minionObject = generateMinion(cardId);
   const CARD_ITEM = generateBoardSlotObject(cardId);
 
   // subtract the card's cost from player's current energy count
-  const newEnergyVal = subtract(energy[ctx.currentPlayer].current, cardCost);
+  const newEnergyVal = subtract(energy[currentPlayer].current, cardCost);
   setCurrentEnergy(G, currentPlayer, newEnergyVal);
 
   // place card in selected index on your board
-  const newBoard = [
-    ...boards[currentPlayer].slice(0, index + 1),
-    CARD_ITEM,
-    ...boards[currentPlayer].slice(index + 1)
-  ];
-
-  // swap new board in
-  boards[currentPlayer] = newBoard;
+  placeCardOnBoard(G, currentPlayer, CARD_ITEM, index);
 
   // move to your playerCards array
   moveCardToPlayedCards(G, currentPlayer, cardId);
