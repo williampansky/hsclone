24b1962ea07787cd6cdfab3c77dad07d22825a1c
William Pansky <williampansky@gmail.com>
2020-02-20 16:55:25 -0600

    added CORE_003 boon

--- src/lib/boons/core.boons.recalculate.js
+++ src/lib/boons/core.boons.recalculate.js
@@ -1,8 +1,33 @@
+import getCardByID from 'lib/utils/get-card-by-id';
+
 const recalculateCoreBoon = (G, player, cardId, index) => {
   // prettier-ignore
   switch (cardId) {
+    case 'CORE_003':  return CORE_003(G, player, cardId);
     case 'CORE_019':  return CORE_019(G, player, cardId);
     default:          break;
+  }
+};
+
+/**
+ * Your other minions of this type have +1 Attack.
+ */
+const CORE_003 = (G, player, cardId) => {
+  const MINION_TYPE = getCardByID(cardId).type;
+
+  const transformEach = (G, player, index) => {
+    G.boards[player][index] = {
+      ...G.boards[player][index],
+      currentAttack: G.boards[player][index].currentAttack - 1,
+      totalAttack: G.boards[player][index].totalAttack - 1
+    };
+  };
+
+  for (let i = 0; i < G.boards[player].length; i++) {
+    if (G.boards[player][i].minionData.id !== cardId) {
+      if (G.boards[player][i].minionData.type === MINION_TYPE)
+        transformEach(G, player, i);
+    }
   }
 };
 
