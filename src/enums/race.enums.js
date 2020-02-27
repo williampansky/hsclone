af1320f55334cfc93edce64374ec548ef41a8837
William Pansky <williampansky@gmail.com>
2020-02-27 10:39:17 -0600

    created RACE enum file

---
+++ src/enums/race.enums.js
@@ -0,0 +1,14 @@
+const RACE = {
+  1: 'NONE',
+  2: 'CREATURE',
+  3: 'PRIMORDIAL',
+  4: 'UNDEAD',
+  5: 'ROBOT',
+  6: 'Demon(temp)',
+  7: 'Murloc(temp)',
+  8: 'Totem(temp)',
+  9: 'Pirate(temp)',
+  10: 'Dragon(temp)'
+};
+
+export default RACE;
