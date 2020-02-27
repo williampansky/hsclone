086ab1ba1102e8b4974f108e7d6ba0acf32c4798
William Pansky <williampansky@gmail.com>
2020-02-27 08:48:25 -0600

    reset hovering-card-index file back 1 commit

--- src/lib/state/hovering-card-index.js
+++ src/lib/state/hovering-card-index.js
@@ -3,35 +3,25 @@ const hoveringCardIndex = {
     '0': null,
     '1': null
   },
+  set: (G, ctx, index) => setHoveringCardIndex(G, ctx, index)
+};
 
-  /**
-   * Sets the value of `hoveringCardIndex` if `index` is provided;
-   * otherwise sets the value to null.
-   *
-   * @param {{}} G
-   * @param {{}} ctx
-   * @param {number} index
-   */
-  set: (G, ctx, index) => {
-    const { selectedCardIndex } = G;
-    const { currentPlayer } = ctx;
+/**
+ * Sets the value of `hoveringCardIndex` if `index` is provided;
+ * otherwise sets the value to null.
+ *
+ * @param {{}} G
+ * @param {{}} ctx
+ * @param {number} index
+ */
+export const setHoveringCardIndex = (G, ctx, index = null) => {
+  const { selectedCardIndex } = G;
+  const { currentPlayer } = ctx;
 
-    if (selectedCardIndex[currentPlayer] !== null)
-      G.hoveringCardIndex[currentPlayer] = null;
-
-    G.hoveringCardIndex[currentPlayer] = index;
-  },
-
-  /**
-   * Resets the currentPlayer's `hoveringCardIndex` value.
-   *
-   * @param {{}} G
-   * @param {{}} ctx
-   */
-  reset: (G, ctx) => {
-    const { currentPlayer } = ctx;
+  if (selectedCardIndex[currentPlayer] !== null)
     G.hoveringCardIndex[currentPlayer] = null;
-  }
+
+  G.hoveringCardIndex[currentPlayer] = index;
 };
 
 export default hoveringCardIndex;
