13c0e5b2b76279c70aa1484a19b757be635915d5
William Pansky <williampansky@gmail.com>
2020-03-02 11:02:53 -0600

    whitespace fix

--- src/lib/moves/init-class-skill.js
+++ src/lib/moves/init-class-skill.js
@@ -30,18 +30,16 @@ const initClassSkill = (G, ctx) => {
 
   // prettier-ignore
   switch (playerClass[currentPlayer]) {
-    case CARDCLASS[1]: return initTargetedDamage(G, ctx);   // AUGUR
-    case CARDCLASS[2]: return backstabOpponent(G, ctx);     // ASSASSIN
-    case CARDCLASS[3]: return iniTargetedSpell(G, ctx);     // SORCERER
-    case CARDCLASS[4]: return summonKnight(G, ctx);         // KNIGHT COMMANDER
-    case CARDCLASS[5]: return initTargetedHeal(G, ctx);     // WHITE MAGE
-    case CARDCLASS[6]: return equipShuriken(G, ctx);        // NINJA
-    case CARDCLASS[7]: return summonRandomIdol(G, ctx);     // MYSTIC
-    case CARDCLASS[8]: return tradeHealthForCard(G, ctx);   // NECROMANCER
-    case CARDCLASS[9]: return gainShieldPoints(G, ctx);     // BERSERKER
-
-    default:
-      return;
+    case CARDCLASS[1]: return initTargetedDamage(G, ctx); // AUGUR
+    case CARDCLASS[2]: return backstabOpponent(G, ctx);   // ASSASSIN
+    case CARDCLASS[3]: return iniTargetedSpell(G, ctx);   // SORCERER
+    case CARDCLASS[4]: return summonKnight(G, ctx);       // KNIGHT COMMANDER
+    case CARDCLASS[5]: return initTargetedHeal(G, ctx);   // WHITE MAGE
+    case CARDCLASS[6]: return equipShuriken(G, ctx);      // NINJA
+    case CARDCLASS[7]: return summonRandomIdol(G, ctx);   // MYSTIC
+    case CARDCLASS[8]: return tradeHealthForCard(G, ctx); // NECROMANCER
+    case CARDCLASS[9]: return gainShieldPoints(G, ctx);   // BERSERKER
+    default: return;
   }
 };
 
