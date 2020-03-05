2aa789b8e709e75e0715215706b8bef728b28fce
William Pansky <williampansky@gmail.com>
2020-03-04 21:43:36 -0600

    updates

--- src/lib/moves/cast-targeted-spell-effect.js
+++ src/lib/moves/cast-targeted-spell-effect.js
@@ -1,33 +1,34 @@
+import { discardCardFromHandByIndex } from 'lib/moves/discard-card';
+import boards from 'lib/state/boards';
+import copyCardToPlayedCards from 'lib/utils/copy-card-to-played-cards';
+import counts from 'lib/state/counts';
+import deselectCard from './deselect-card';
+import drawCard from './draw-card';
+import energy from 'lib/state/energy';
+import health from 'lib/state/health';
 import playerCanBeAttacked from 'lib/state/player-can-be-attacked';
 import playerCanBeHealed from 'lib/state/player-can-be-healed';
-import boards from 'lib/state/boards';
-import energy from 'lib/state/energy';
-import deselectCard from './deselect-card';
-import copyCardToPlayedCards from 'lib/utils/copy-card-to-played-cards';
 import removeCardFromHand from 'lib/utils/remove-card-from-hand';
-import counts from 'lib/state/counts';
-import drawCard from './draw-card';
-import health from 'lib/state/health';
-import { discardCardFromHandByIndex } from 'lib/moves/discard-card';
 import WARCRY_TARGET_CONTEXT from 'enums/warcry.target-context.enum';
 
 /**
  * Casts a targeted Warcry spell object.
+ *
  * @param {{}} G
  * @param {{}} ctx
  * @param {string} playerCtx Either SELF || OPPONENT
  * @param {string} targetCtx Target context; MINION || PLAYER
- * @param {string} targetIdx Target index if targetCtx is MINION
+ * @param {string} index Target index if targetCtx is MINION
  */
-const castTargetedSpellEffect = (G, ctx, playerCtx, targetCtx, targetIdx) => {
+const castTargetedSpellEffect = (G, ctx, playerCtx, targetCtx, index) => {
   const { selectedCardObject, turnOrder } = G;
-  const { currentPlayer } = ctx;
+  const { currentPlayer, random } = ctx;
   const otherPlayer = turnOrder.find(p => p !== currentPlayer);
 
   if (selectedCardObject[currentPlayer] === null) return;
   const { id, cost, uuid } = selectedCardObject[currentPlayer];
-  const THEIR_SLOT = G.boards[otherPlayer][targetIdx];
-  const YOUR_SLOT = G.boards[otherPlayer][targetIdx];
+  const THEIR_SLOT = G.boards[otherPlayer][index];
+  const YOUR_SLOT = G.boards[currentPlayer][index];
 
   energy.subtract(G, currentPlayer, cost);
   deselectCard(G, ctx);
@@ -58,78 +59,75 @@ const castTargetedSpellEffect = (G, ctx, playerCtx, targetCtx, targetIdx) => {
   boards.disableAllCanBeHealed(G, '0');
   boards.disableAllCanBeHealed(G, '1');
 
-  // prettier-ignore
   switch (id) {
-    case 'CORE_115':  return CORE_115(G, ctx, currentPlayer, otherPlayer, THEIR_SLOT, targetIdx);
-    case 'CORE_116':  return CORE_116(G, ctx, currentPlayer, otherPlayer, THEIR_SLOT, targetCtx, targetIdx);
-    case 'CORE_119':  return CORE_119(G, ctx, currentPlayer, otherPlayer, THEIR_SLOT, targetCtx, targetIdx);
-    case 'CORE_120':  return CORE_120(G, ctx, otherPlayer, THEIR_SLOT, targetIdx);
-    case 'CORE_123':  return CORE_123(G, currentPlayer, targetIdx);
-    case 'CORE_126':  return CORE_126(G, ctx, otherPlayer, THEIR_SLOT, targetIdx);
-    default:          return;
+    case 'CORE_044':
+      boards.subtractFromMinionHealth(G, otherPlayer, index, 1);
+      boards.killMinionIfHealthIsZero(G, ctx, otherPlayer, THEIR_SLOT, index);
+      break;
+
+    case 'CORE_046':
+      G.boards[currentPlayer][index] = {
+        ...YOUR_SLOT,
+        currentAttack: YOUR_SLOT.currentAttack + 2,
+        currentHealth: YOUR_SLOT.currentHealth + 2,
+        hasGuard: true,
+        totalAttack: YOUR_SLOT.totalAttack + 2,
+        totalHealth: YOUR_SLOT.totalHealth + 2
+      };
+      break;
+
+    case 'CORE_056':
+      G.boards[otherPlayer][index].currentHealth = 1;
+      break;
+
+    case 'CORE_115':
+      boards.subtractFromMinionHealth(G, otherPlayer, index, 1);
+      boards.killMinionIfHealthIsZero(G, ctx, otherPlayer, THEIR_SLOT, index);
+      if (THEIR_SLOT.currentHealth === 0) drawCard(G, ctx, currentPlayer, 1);
+      break;
+
+    case 'CORE_116':
+      if (targetCtx === WARCRY_TARGET_CONTEXT[2]) {
+        health.subtract(G, otherPlayer, 4);
+      } else {
+        boards.subtractFromMinionHealth(G, otherPlayer, index, 4);
+        boards.killMinionIfHealthIsZero(G, ctx, otherPlayer, THEIR_SLOT, index);
+      }
+
+      discardCardFromHandByIndex(
+        G,
+        currentPlayer,
+        random.Die(G.players[currentPlayer].hand.length)
+      );
+      break;
+
+    case 'CORE_119':
+      if (targetCtx === WARCRY_TARGET_CONTEXT[2]) {
+        health.subtract(G, otherPlayer, 2);
+      } else {
+        boards.subtractFromMinionHealth(G, otherPlayer, index, 2);
+        boards.killMinionIfHealthIsZero(G, ctx, otherPlayer, THEIR_SLOT, index);
+      }
+
+      health.add(G, currentPlayer, 2);
+      break;
+
+    case 'CORE_120':
+      boards.subtractFromMinionHealth(G, otherPlayer, index, 4);
+      boards.killMinionIfHealthIsZero(G, ctx, otherPlayer, THEIR_SLOT, index);
+      break;
+
+    case 'CORE_123':
+      G.boards[currentPlayer][index].canAttack = true;
+      break;
+
+    case 'CORE_126':
+      boards.killMinion(G, ctx, otherPlayer, THEIR_SLOT, index);
+      break;
+
+    default:
+      break;
   }
-};
-
-const CORE_115 = (G, ctx, currentPlayer, otherPlayer, boardSlot, index) => {
-  boards.subtractFromMinionHealth(G, otherPlayer, index, 1);
-  boards.killMinionIfHealthIsZero(G, ctx, otherPlayer, boardSlot, index);
-  if (boardSlot.currentHealth === 0) drawCard(G, ctx, currentPlayer, 1);
-};
-
-const CORE_116 = (
-  G,
-  ctx,
-  currentPlayer,
-  otherPlayer,
-  boardSlot,
-  targetCtx,
-  index
-) => {
-  const AMOUNT = 4;
-  const randomIdx = ctx.random.Die(G.players[currentPlayer].hand.length);
-
-  if (targetCtx === WARCRY_TARGET_CONTEXT[2]) {
-    health.subtract(G, otherPlayer, AMOUNT);
-  } else {
-    boards.subtractFromMinionHealth(G, otherPlayer, index, AMOUNT);
-    boards.killMinionIfHealthIsZero(G, ctx, otherPlayer, boardSlot, index);
-  }
-
-  discardCardFromHandByIndex(G, currentPlayer, randomIdx);
-};
-
-const CORE_119 = (
-  G,
-  ctx,
-  currentPlayer,
-  otherPlayer,
-  boardSlot,
-  targetCtx,
-  index
-) => {
-  const AMOUNT = 2;
-
-  if (targetCtx === WARCRY_TARGET_CONTEXT[2]) {
-    health.subtract(G, otherPlayer, AMOUNT);
-  } else {
-    boards.subtractFromMinionHealth(G, otherPlayer, index, AMOUNT);
-    boards.killMinionIfHealthIsZero(G, ctx, otherPlayer, boardSlot, index);
-  }
-
-  health.add(G, currentPlayer, AMOUNT);
-};
-
-const CORE_120 = (G, ctx, player, boardSlot, index) => {
-  boards.subtractFromMinionHealth(G, player, index, 4);
-  boards.killMinionIfHealthIsZero(G, ctx, player, boardSlot, index);
-};
-
-const CORE_123 = (G, player, index) => {
-  G.boards[player][index].canAttack = true;
-};
-
-const CORE_126 = (G, ctx, player, boardSlot, index) => {
-  return boards.killMinion(G, ctx, player, boardSlot, index);
 };
 
 export default castTargetedSpellEffect;
