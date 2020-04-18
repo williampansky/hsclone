e0f6eba4c07ae8f3dcc6ecf45bbac32c24528005
William Pansky <williampansky@gmail.com>
2020-04-18 18:03:50 -0500

    added mechanic comments to enums file

--- src/enums/mechanics.enums.js
+++ src/enums/mechanics.enums.js
@@ -1,35 +1,35 @@
 // prettier-ignore
 const MECHANICS = {
-  1:  '%BOON%',           // buffs your minions
-  2:  '%BUFF%',           // buffs your player
+  1:  '%BOON%',           // temporary bonuses
+  2:  '%BUFF%',           // permanent bonuses
   3:  '%ON_DEATH%',       // activated when minion dies
-  4:  '%BULWARK%',        // only this & other guard minions can be attacked
-  5:  '%RUSH%',           // minion can attack immediately
+  4:  '%BULWARK%',        // only these minions can be attacked
+  5:  '%RUSH%',           // can attack immediately
   6:  '%ON_PLAY%',        // activated when card is played
-  7:  '%DOUBLE_ATTACK%',  // player/minion can attack twice
-  8:  '%HIDDEN%',         // minion cannot be attacked or targeted
-  9:  '%BUBBLE%',         // minion cannot lose Health with E.S. active
+  7:  '%DOUBLE_ATTACK%',  // can attack twice
+  8:  '%HIDDEN%',         // cannot be attacked or targeted
+  9:  '%BUBBLE%',         // cannot lose Health when active
   10: '%EXPIRATION%',     // minion will be killed at start of your next turn
-  11: '%DISABLE%',        // minion cannot attack while disabled
-  12: '%ARMOR%',
-  13: '%ENERGY%',
-  14: '%CANT_TARGET%',
-  15: '%COMBO%',
-  16: '%DEBUFF%',
-  17: '%DESTROY%',
-  18: '%DISCOVER%',
-  19: '%DRAW_CARD%',
-  20: '%IMMUNE%',
-  21: '%LIFESTEAL%',
-  22: '%MYSTERY%',
-  23: '%OVERLOAD%',
-  24: '%POISON%',
-  25: '%SET_VALUE%',
-  26: '%SILENCE%',
-  27: '%SPELL_DAMAGE%',
-  28: '%STEAL%',
-  29: '%SUMMON%',
-  30: '%TRANSFORM%'
+  11: '%DISABLE%',        // minion cannot attack
+  12: '%ARMOR%',          // secondary HP available only to players
+  13: '%ENERGY%',         // resource used to play cards
+  14: '%CANT_TARGET%',    // unable to be targeted by spells, skills, or items
+  15: '%COMBO%',          // triggers if you already played another card
+  16: '%DEBUFF%',         // permanent bonus descrease
+  17: '%DESTROY%',        // kills target
+  18: '%DISCOVER%',       // choose 1 from 3 random class-relative cards
+  19: '%DRAW_CARD%',      // draw one or more cards from player's deck
+  20: '%IMMUNE%',         // prevents all damage
+  21: '%LIFESTEAL%',      // heals for amount of damage dealt
+  22: '%MYSTERY%',        // hidden spell that triggers under circumstances
+  23: '%OVERLOAD%',       // reduces total energy resource next turn
+  24: '%POISON%',         // kill any minion damaged by this mechanic
+  25: '%SET_VALUE%',      // set attribute to a certain value
+  26: '%SILENCE%',        // removes all buffs/debuffs/etc
+  27: '%SPELL_DAMAGE%',   // increases damage done by spells
+  28: '%STEAL%',          // take control of target
+  29: '%SUMMON%',         // generates a minion
+  30: '%TRANSFORM%'       // transforms target into something else
 };
 
 export default MECHANICS;
