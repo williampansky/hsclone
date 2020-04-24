1128ebda90e8bb6ae2211c6d2fbf1653e3eb3b0c
William Pansky <williampansky@gmail.com>
2020-04-24 14:33:37 -0500

    swapped renders

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import './styles/game.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
 if (process.env.NODE_ENV !== 'production' && module.hot) {
   module.hot.accept('./App', () => {
     const App = require('./App').default;
     ReactDOM.render(<App />, document.getElementById('root'));
   });
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
